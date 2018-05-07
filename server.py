2ec7bed5fdc8c3946a30d55cf6d04c291938328e
tonny62 <ton_kriddanai@hotmail.com>
2018-05-07 16:58:59 +0700

    initial commit

---
+++ server.py
@@ -0,0 +1,16 @@
+import socketserver
+
+class MyTCPHandler(socketserver.BaseRequestHandler):
+    def handle(self):
+        self.data = self.request.recv(1024).strip()
+        print(self.data)
+        self.request.sendall(self.data.upper())
+
+if __name__ == "__main__":
+    HOST, PORT = "localhost", 9999
+
+    # Create the server, binding to localhost on port 9999
+    with socketserver.TCPServer((HOST, PORT), MyTCPHandler) as server:
+        # Activate the server; this will keep running until you
+        # interrupt the program with Ctrl-C
+        server.serve_forever()
