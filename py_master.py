d32a88d3e09e22639a3a548722bc7afe9c18474f
Kittikom Sangrit <sangrit@Sangrits-MacBook-Pro.local>
2018-05-31 13:23:53 +0700

    developing deSerializeState

--- py_master.py
+++ py_master.py
@@ -1,13 +1,53 @@
 from mpi4py import MPI
+from shared_folder import datastructure
 import numpy
 import sys
-comm = MPI.COMM_SELF.Spawn(sys.executable,
-            args=['py_slave.py'],
-                maxprocs=5)
-N = numpy.array(100, 'i')
-comm.Bcast([N, MPI.INT], root=MPI.ROOT)
-PI = numpy.array(0.0, 'd')
-comm.Reduce(None, [PI, MPI.DOUBLE],
-            op=MPI.SUM, root=MPI.ROOT)
-print(PI)
-comm.Disconnect()
+import socket
+
+def deSerializeState(size,listBoard):
+    rows = []
+    for i in range(size):
+        row = []
+        for j in range(size):
+            row.append(listBoard[i*size+j])
+        print(row)
+        rows.append(row)
+    return rows
+
+
+def main():
+    PORT = 50007              # Arbitrary non-privileged port
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((HOST, PORT)),nn
+    while 1 :
+        s.listen()
+        conn, addr = s.accept()
+        with conn:
+            print('Connected by', addr)
+            while True:
+                data = conn.recv(1024)
+                if not data: break
+                data = data.decode()
+                data = data.split(',') # data[0] is board size, data[1] is board data
+                board = deSerializeState(data[0],data[1])
+
+                ## data = data + "go UP"
+                ## data = bytes("Welcome to my chat server", encoding='utf-8')
+                conn.sendall(data)
+
+if __name__ == "__main__" :
+    main()
+
+'''
+    comm =  MPI.COMM_SELF.Spawn(
+                sys.executable,
+                args=['py_slave.py'],
+                maxprocs=5
+            )
+    N = numpy.array(100, 'i')
+    comm.Bcast([N, MPI.INT], root=MPI.ROOT)
+    PI = numpy.array(0.0, 'd')
+    comm.Reduce(None, [PI, MPI.DOUBLE], op=MPI.SUM, root=MPI.ROOT)
+    print(PI)
+    comm.Disconnect()
+'''
